22f421a46cb93c3e8e1f6603705531606dabc88d
Yixuan Guo <gyixuan@vmware.com>
2020-08-11 09:44:27 +0800

    Finished TwoSum problem.

--- 1.TwoSum/src/com/company/Main.java
+++ 1.TwoSum/src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
+import java.util.*;
 
 public class Main {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> dict = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (dict.containsKey(diff)) {
+                return new int[]{dict.get(diff), i};
+            } else {
+                dict.put(nums[i], i);
+            }
+        }
+        throw new IllegalArgumentException("no solution");
+    }
 
     public static void main(String[] args) {
-	// write your code here
+	    int num[] = {2, 7, 11, 15};
+        int target = 9;
+        int result[]= twoSum(num, target);
+        for(int i =0;i< result.length;i++){
+            System.out.println(result[i]);
+        }
     }
 }
