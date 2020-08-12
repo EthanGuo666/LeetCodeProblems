6753f0fb467098e489ab277239479151121b830c
Yixuan Guo <gyixuan@vmware.com>
2020-08-12 10:25:43 +0800

    Created project 3-LongestSubstring.

---
+++ 3.LongestSubstring/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    public static int lengthOfLongestSubstring(String s) {
+        String str = s;
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String testStr = "helohelo";
+        int result = lengthOfLongestSubstring(testStr);
+        System.out.println(result);
+    }
+}
