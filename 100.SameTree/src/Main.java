db5d8f050035f55d5583a56cca28cec1c010acdd
Yixuan Guo <2761683494@qq.com>
2021-06-21 22:23:51 +0800

    Need more practice on recursive method

---
+++ 100.SameTree/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+
+    public static void main(String[] args) {
+//      树到底该怎么写
+//        TreeNode pp = [1,2,3];
+//        TreeNode qq = [1,2,3];
+
+    }
+}
