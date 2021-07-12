d7f31abd002e3f6250c23f6bdc725176adecdbf5
Yixuan Guo <2761683494@qq.com>
2021-07-12 19:37:49 +0800

    Got some inspirations in recursive problems

---
+++ 112.PathSum/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        if (root.left == null && root.right != null) {
+            return hasPathSum(root.right, targetSum - root.val);
+        }
+        if (root.left != null && root.right == null) {
+            return hasPathSum(root.left, targetSum - root.val);
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
