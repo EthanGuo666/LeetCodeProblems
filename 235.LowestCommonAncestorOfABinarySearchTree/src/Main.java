e8855409f5473b177ff7660f79a324350bf9d208
Yixuan Guo <2761683494@qq.com>
2021-07-14 10:51:30 +0800

    Make use of the features of BST!!

---
+++ 235.LowestCommonAncestorOfABinarySearchTree/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root==null) {
+            return null;
+        }
+        if (root.val>Math.max(p.val, q.val)) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val<Math.min(p.val, q.val)) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
