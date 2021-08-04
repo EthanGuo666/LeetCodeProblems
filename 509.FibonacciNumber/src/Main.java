73c3a511e5c1297c33a81fbfcb333c3f50f6f925
Yixuan <2761683494@qq.com>
2021-08-04 10:41:23 +0800

    easy

---
+++ 509.FibonacciNumber/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        int[] result = new int[n + 1];
+        result[0] = 0;
+        result[1] = 1;
+        result[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        return result[n];
+    }
+}
