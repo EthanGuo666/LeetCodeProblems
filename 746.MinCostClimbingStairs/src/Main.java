4f0a02fe80397d454cdbb1fb692faf670364a44f
Yixuan <2761683494@qq.com>
2021-09-07 20:18:56 +0800

    easy

--- 746.MinCostClimbingStairs/src/Main.java
+++ 746.MinCostClimbingStairs/src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public int minCostClimbingStairs(int[] cost) {
         int len = cost.length;
-        int[] newCost = new int[len + 1];
-        newCost[0] = newCost[1] = 0;
-        for (int i = 2; i <= len; i++) {
-            newCost[i] = Math.min(cost[i - 1] + newCost[i - 1], cost[i - 2] + newCost[i - 2]);
+        int[] arr = new int[len + 2];
+        int[] res = new int[len + 2];
+        for (int i = 1; i <= len; i++) {
+            arr[i] = cost[i - 1];
         }
-        return newCost[len];
+        res[1] = arr[1];
+        for (int i = 2; i <= len + 1; i++) {
+            res[i] = Math.min(res[i - 2] + arr[i], res[i - 1] + arr[i]);
+        }
+        return res[len + 1];
     }
 }
