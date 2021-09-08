630b1603fed933d832c2bde3feaf111067bf0694
Yixuan <2761683494@qq.com>
2021-09-08 10:13:41 +0800

    和198一样

--- 213.HouseRobber-II/src/Main.java
+++ 213.HouseRobber-II/src/Main.java
@@ -1,39 +1,50 @@
 public class Main {
-    public static int find(int[] copy, int len) {
-        int[] ans = new int[len + 2];
-        int max = 0;
-        for (int i = 2; i < len + 1; i++) {
-            ans[i] = Math.max(ans[i - 1], copy[i] + ans[i - 2]);
-            max = Math.max(max, ans[i]);
-        }
-        return max;
-    }
-
     public static int rob(int[] nums) {
         int len = nums.length;
         if (len == 1) {
             return nums[0];
         }
-        if (len == 2) {
-            return Math.max(nums[0], nums[1]);
-        }
-        // 因为第一个和最后一个元素不可能同时出现，那就分别跑两次最后取最大值
-        int[] copy = new int[len + 1];
-        // 从nums数组第一个元素开始复制，到nums倒数第二个 （相当于跳过最后一个）
+        // 做两次，一次没有nums[0], 一次没有nums[len-1]
+        int[] arr = new int[len + 1];
+        int[] res = new int[len + 1];
         for (int i = 2; i < len + 1; i++) {
-            copy[i] = nums[i - 2];
+            arr[i] = nums[i - 2];
         }
-        int max = find(copy, len);
+        res[2] = arr[2];
+        int max = arr[2];
+        for (int i = 3; i < len + 1; i++) {
+            res[i] = Math.max(res[i - 3] + arr[i], res[i - 2] + arr[i]);
+            if (max < res[i]) {
+                max = res[i];
+            }
+        }
 
-        // 从nums数组第二个元素开始复制，到最后一个 （相当于跳过第一个）
         for (int i = 2; i < len + 1; i++) {
-            copy[i] = nums[i - 1];
+            arr[i] = nums[i - 1];
         }
-        max = Math.max(max, find(copy, len));
+
+        res[2] = arr[2];
+        if (max < arr[2]) {
+            max = arr[2];
+        }
+        for (int i = 3; i < len + 1; i++) {
+            res[i] = Math.max(res[i - 3] + arr[i], res[i - 2] + arr[i]);
+            if (max < res[i]) {
+                max = res[i];
+            }
+        }
+
+//        for (int i = 0; i < len + 1; i++) {
+//            System.out.println(arr[i]);
+//        }
         return max;
     }
 
     public static void main(String[] args) {
-
+        // {2,3,2}       3
+        // {1,2,3,1}     4
+        // {1,2,3}       3
+        int[] nums = {1,2,3};
+        System.out.println(rob(nums));
     }
 }
