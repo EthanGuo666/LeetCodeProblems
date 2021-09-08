5e83fc730b51483b1b2aecbdb8164c318ac76cb7
Yixuan <2761683494@qq.com>
2021-09-08 11:22:13 +0800

    再做一次，注意用数组下标记录

--- 740.DeleteAndEarn/src/Main.java
+++ 740.DeleteAndEarn/src/Main.java
@@ -1,38 +1,39 @@
 public class Main {
     public static int deleteAndEarn(int[] nums) {
-        int maxVal = 0;
-        for (int val : nums) {
-            maxVal = Math.max(maxVal, val);
-        }
-        int[] sum = new int[maxVal + 1];
-        // 这里非常巧妙地用数组下标i来记录所有数字i相加的和
-        for (int val : nums) {
-            sum[val] += val;
-        }
-        return rob(sum);
-    }
-
-    public static int rob(int[] nums) {
+        int numsMax = -1;
         int len = nums.length;
         if (len == 1) {
             return nums[0];
         }
-        if (len == 2) {
-            return Math.max(nums[0], nums[1]);
+        for (int i = 0; i < len; i++) {
+            if (numsMax < nums[i]) {
+                numsMax = nums[i];
+            }
         }
-        int[] ans = new int[len];
-        ans[0] = 0;
-        ans[1] = nums[1];
-        int max = nums[1];
-        for (int i = 2; i < len; i++) {
-            ans[i] = Math.max(ans[i - 2] + nums[i], ans[i - 1]);
-            max = Math.max(max, ans[i]);
+        int[] arr = new int[numsMax + 2];
+        int[] res = new int[numsMax + 2];
+        // 把相同数的求和，存在对应下标+1的位置
+        for (int i = 0; i < len; i++) {
+            arr[nums[i] + 1] += nums[i];
+        }
+        res[2] = arr[2];
+        int max = res[2];
+        for (int i = 3; i < numsMax + 2; i++) {
+            res[i] = Math.max(res[i - 2] + arr[i], res[i - 3] + arr[i]);
+            if (max < res[i]) {
+                max = res[i];
+            }
         }
         return max;
     }
 
     public static void main(String[] args) {
-        int[] nums = {2, 2, 3, 5, 4};
+        // {1,1,1,1,2}     4
+        // {1,2}           2
+        // {1,3}           4
+        // {1,4}           5
+        // {2,2,3,3,3,4}   9
+        int[] nums = {2,2,3,3,3,4};
         //deleteAndEarn(nums);
         System.out.println(deleteAndEarn(nums));
     }
