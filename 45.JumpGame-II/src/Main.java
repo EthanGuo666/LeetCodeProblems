e95af54f2bf29e624ec3657fbc655c1d10e7c7d1
Yixuan <2761683494@qq.com>
2021-09-09 09:41:29 +0800

    必须重做，学习简洁的风格

--- 45.JumpGame-II/src/Main.java
+++ 45.JumpGame-II/src/Main.java
@@ -1,19 +1,31 @@
 public class Main {
-    public int jump(int[] nums) {
+    public static int jump(int[] nums) {
         int len = nums.length;
-        int rightMost = 0;
-        int mostPos = 0;
-        int step = nums[0];
-        for(int i=0;i<len;i++){
-
-            if (i<rightMost){
-                rightMost = Math.max(rightMost, nums[i]+i);
+        int steps = 0;
+        int maxPos = 0;
+        // 用一个end指向跳变末尾
+        int end = 0;
+        for (int i = 0; i < len - 1; i++) {
+            maxPos = Math.max(maxPos, i + nums[i]);
+            if (i == end) {
+                end = maxPos;
+                steps++;
             }
-
         }
+        return steps;
     }
 
     public static void main(String[] args) {
-
+        // {1}               0
+        // {1,2}             1
+        // {2,1}             1
+        // {1,2,3}           2
+        // {2,2,3,1,2}       2
+        // {1,1,1,1,1}       4
+        // {2,3,0,1,4}       2
+        // {1,2,0,1}         2
+        // {1,1,2,1,1}       3
+        int[] nums = {1, 1, 2, 1, 1};
+        System.out.println(jump(nums));
     }
 }
