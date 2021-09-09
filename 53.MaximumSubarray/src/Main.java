18a9d4044f0077a4f371a0c9fbea8fd4232fde03
Yixuan <2761683494@qq.com>
2021-09-09 11:17:00 +0800

    重做，学习简洁风格

--- 53.MaximumSubarray/src/Main.java
+++ 53.MaximumSubarray/src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static int maxSubArray(int[] nums) {
-        int preSum = 0;
-        int maxSum = nums[0];
-        for (int i = 0; i < nums.length; i++) {
-            preSum = Math.max(preSum + nums[i], nums[i]);
-            maxSum = Math.max(preSum, maxSum);
+        int pre=0, max=nums[0];
+        for(int i=0;i<nums.length;i++){
+            pre=Math.max(nums[i], pre+nums[i]);
+            max=Math.max(pre,max);
         }
-        return maxSum;
+        return max;
     }
 
     public static void main(String[] args) {
-        int[] numbers = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        // {-2,1,-3,4,-1,2,1,-5,4}    6
+        // {1,1,1}                    3
+        // {1}                        1
+        //
+        int[] numbers = {0};
         System.out.println(maxSubArray(numbers));
     }
 }
