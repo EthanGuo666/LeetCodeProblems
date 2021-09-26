9cee4bd701055f59f6bd0520ccaf2c3024b45123
Yixuan <2761683494@qq.com>
2021-09-26 18:55:44 +0800

    重做，这个题很秀

---
+++ 264.UglyNumberII/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static int nthUglyNumber(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+        for (int i = 2; i <= n; i++) {
+            int num2 = dp[p2] * 2;
+            int num3 = dp[p3] * 3;
+            int num5 = dp[p5] * 5;
+            dp[i] = Math.min(num2, Math.min(num3, num5));
+            if (dp[i] == num2) p2++;
+            if (dp[i] == num3) p3++;
+            if (dp[i] == num5) p5++;
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        // n = 10   ans=12
+        // n = 1    ans=1
+        int n = 1;
+        System.out.println(nthUglyNumber(n));
+    }
+}
